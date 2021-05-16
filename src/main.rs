0b012845df2ce6cdd371c88946298693bd5c9279
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-16 21:33:27 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let content = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Applictaion error: {}", e);
 
-    println!("With text: \n{}", content);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -21,10 +29,22 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", content);
+
+    Ok(())
 }
