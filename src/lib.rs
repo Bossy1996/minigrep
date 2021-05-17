5ad8933f5b267143495da1b59253a893659bfadd
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-17 19:43:02 +0200

    case insensitive fn and test

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@ pub fn search_case_insensitive<'a>(
     let mut results = Vec::new();
 
     for line in contents.lines() {
-        if line.to_lowercase().contains(query) {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
