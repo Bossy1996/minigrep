8fd68560e08b96346893a1bacd598860907a0a28
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-18 15:30:17 +0200

    changed println macro to eprintline

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,13 @@ fn main() {
     println!("{:?}", args);
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
 
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Applictaion error: {}", e);
+        eprintln!("Applictaion error: {}", e);
 
         process::exit(1);
     }
